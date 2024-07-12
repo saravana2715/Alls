f66e65a74dd024668ddb6449befca8a2714a07fe
saravana2715 <dtksaravana@gmail.com>
2024-07-12 10:15:51 +0530

    First commit

---
+++ src/test/java/org/vet/drive.java
@@ -0,0 +1,16 @@
+package org.vet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class drive {
+
+	public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.omrbranch.com/");
+	
+	
+
+	}
+
+}
